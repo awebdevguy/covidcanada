c68990c02af71d86865c8ed670e9b5ddc3e0729c
awebdevguy <awebdevguy@gmail.com>
2022-05-07 08:04:51 -0600

    replaced code to work with new dataset
    replaced index.html, main.js, styles.css

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-// Header information output
 const headerDate = document.getElementById('header-date');
 const headerProv = document.getElementById('header-prov');
 const headerRegion = document.getElementById('header-region');
@@ -10,31 +8,31 @@ const provDropdown = document.getElementById("prov-dropdown");
 const regionDropdown = document.getElementById("region-dropdown");
 var provinceSelected = '';
 
-const region = document.getElementById("region");
 const newDeaths = document.getElementById("new-deaths");
 const totalDeaths = document.getElementById("total-deaths");
 
 const newCase = document.getElementById("new-case");
 const activeCase = document.getElementById("active-case");
 const totalCase = document.getElementById("total-case");
-const newVaccine = document.getElementById("new-vaccine");
-const completedVaccine = document.getElementById("completed-vaccine");
-const totalAdministered = document.getElementById("total-administered");
-const totalDistributed = document.getElementById("total-distributed");
-const newRecovered = document.getElementById("new-recovered");
-const totalRecovered = document.getElementById("total-recovered");
+const newHospital = document.getElementById("new-hospital");
+const totalHospital = document.getElementById("total-hospital");
+const newICU = document.getElementById("new-icu");
+const totalICU = document.getElementById("total-icu");
+const newTest = document.getElementById("new-test");
+const totalTests = document.getElementById("total-tests");
 
 // const proxyURL = "https://quicors.herokuapp.com/";
 const proxyURL = '';
 
-const provincesURL =  proxyURL + "https://api.opencovid.ca/other?stat=prov";
-const healthRegionsURL = proxyURL + "https://api.opencovid.ca/other?stat=hr";
-const timeseriesURL = proxyURL + "https://api.opencovid.ca/timeseries";
-const summaryURL = proxyURL + "https://api.opencovid.ca/summary";
-const canadaSummaryURL = proxyURL + "https://api.opencovid.ca/summary?loc=canada";
-const ABCentralSummaryURL = proxyURL + "https://api.opencovid.ca/summary?loc=4833";
-const otherURL = proxyURL + "https://api.opencovid.ca/other";
-const versionURL = proxyURL + "https://api.opencovid.ca/version";
+const provincesURL =  proxyURL + "./assets/provinces.json";
+const healthRegionsURL = proxyURL + "./assets/health_regions.json";
+const canadaSummaryURL = proxyURL + "https://api.opencovid.ca/summary?geo=can";
+const regionsArray = [];
+
+
+console.log(provincesURL);
+console.log(healthRegionsURL);
+console.log(canadaSummaryURL);
 
 // Initialize dropdowns with first option of 'All'
 initDropdownList(provDropdown);
@@ -53,16 +51,20 @@ async function loadProvSelector(url) {
     const results = await res.json() ;
     console.log("results: " + JSON.stringify(results));
 
-    results.prov.forEach(item => {
-      const { province_full, province_short } = item;
+    results.forEach(item => {
+      const { region, name_canonical } = item;
 
-      if(province_short != 'RP') {
+      regionsArray.push(region);
+
+      if(region != 'RP') {
         let opt = document.createElement('option');
-        opt.text = province_full;
-        opt.value = province_short;
+        opt.text = name_canonical;
+        opt.value = region;
         provDropdown.add(opt);
       }
     });
+
+    console.log(regionsArray);
 
   } catch (err) {
     console.log("Error fetching provinces for selector: " + err);
@@ -94,17 +96,17 @@ async function loadRegionSelector(prov) {
     const results = await res.json() ;
     console.log("results: " + JSON.stringify(results));
 
-    results.hr.forEach(item => {
-      const { HR_UID, health_region_esri, province_short } = item;
+    results.forEach(item => {
+      const { hruid, name_canonical, region } = item;
 
-      console.log("HR_UID: " + HR_UID);
-      console.log("health_region_esri: " + health_region_esri);
-      console.log("province_short: " + province_short);
+      console.log("hruid: " + hruid);
+      console.log("name_canonical: " + name_canonical);
+      console.log("region: " + region);
 
-      if(province_short === prov && HR_UID != 9999) {
+      if(region === prov && hruid != 9999) {
         let opt = document.createElement('option');
-        opt.text = health_region_esri;
-        opt.value = HR_UID;
+        opt.text = name_canonical;
+        opt.value = hruid;
         regionDropdown.add(opt);
       }
     });
@@ -115,61 +117,72 @@ async function loadRegionSelector(prov) {
 
 // Put data on screen
 function viewStats(dat) {
-  let data = dat.summary;
-  console.log("test: " + JSON.stringify(data));
 
-  const { date, province, deaths, cumulative_deaths, cases, active_cases, cumulative_cases, recovered, cumulative_recovered, avaccine, cumulative_avaccine, cumulative_cvaccine, cumulative_dvaccine } = data[0];
-
-  const dateString = new Date(convertFromStringToDate(date)).toDateString();
-  console.log("date converted: " + convertFromStringToDate(date));
-
-  headerDate.innerText = dateString;
-  headerProv.innerText = province.toUpperCase();
+  const { date, region, deaths_daily, deaths, cases_daily, cases, hospitalizations_daily, hospitalizations, icu_daily, icu, tests_completed_daily, tests_completed } = dat.data[0];
   
-  if(province === 'Canada') {
+  console.log(date);
+  console.log(region);
+
+  headerDate.innerText = date;
+  headerProv.innerText = region === 'CAN' ? 'CANADA' : region.toUpperCase();
+  
+  if(regionsArray.indexOf(region) === -1) {
     headerRegion.innerText = 'All';
   }
 
   console.log("viewstate date: " + dated);
-  console.log("new_deaths: " + deaths);
+  console.log("new_deaths: " + deaths_daily);
 
-  dated.innerText = dateString;
-  newDeaths.innerText = deaths ? deaths.toLocaleString() : 0;
-  totalDeaths.innerText = cumulative_deaths ? cumulative_deaths.toLocaleString() : 0;
+  const na = "n/a"
 
-  newCase.innerText = cases ? cases.toLocaleString() : 0;
-  activeCase.innerText = active_cases ? active_cases.toLocaleString() : "n/a";
-  totalCase.innerText = cumulative_cases ? cumulative_cases.toLocaleString() : "n/a";
-  newVaccine.innerText = avaccine ? avaccine.toLocaleString() : "n/a";
-  completedVaccine.innerText = cumulative_cvaccine ? cumulative_cvaccine.toLocaleString() : "n/a";
-  totalAdministered.innerText = cumulative_avaccine ? cumulative_avaccine.toLocaleString() : "n/a";
-  totalDistributed.innerText = cumulative_dvaccine ? cumulative_dvaccine.toLocaleString() : "n/a";
-  newRecovered.innerText = recovered ? recovered.toLocaleString() : "n/a";
-  totalRecovered.innerText = cumulative_recovered ? cumulative_recovered.toLocaleString() : "n/a";
+  dated.innerText = date;
+  newDeaths.innerText = deaths_daily ? deaths_daily.toLocaleString() : na;
+  totalDeaths.innerText = deaths ? deaths.toLocaleString() : 0;
+
+  newCase.innerText = cases_daily ? cases_daily.toLocaleString() : na;
+  totalCase.innerText = cases ? cases.toLocaleString() : na;
+  newHospital.innerText = hospitalizations_daily ? hospitalizations_daily.toLocaleString() : na;
+  totalHospital.innerText = hospitalizations ? hospitalizations.toLocaleString() : na;
+  newICU.innerText = icu_daily ? icu_daily.toLocaleString() : na;
+  totalICU.innerText = icu ? icu.toLocaleString() : na;
+  newTest.innerText = tests_completed_daily ? tests_completed_daily.toLocaleString() : na;
+  totalTests.innerText = tests_completed ? tests_completed.toLocaleString() : na;
 }
 
 // Get province records and call function that loads region dropdown list
 function getProvince(value) {
   initDropdownList(regionDropdown);
   provinceSelected = value === 'All' ? 'canada' : value;
-  const url = `${proxyURL}https://api.opencovid.ca/summary?loc=${provinceSelected}`;
+  let url;
+
+  if(provinceSelected === 'canada') {
+    url = `${proxyURL}https://api.opencovid.ca/summary?geo=can`;
+  } else {
+    url = `${proxyURL}https://api.opencovid.ca/summary?loc=${provinceSelected}&pt_names=canonical&hr_names=canonical`;
+  }
+
   loadData(url);
-  console.log("url: " + url);
   loadRegionSelector(value);
   headerRegion.innerText = regionDropdown.options[0].text;
 }
 
 // Get health region
 function getRegion(value) {
-  let region = value === 'All' ? provinceSelected : value;
-  const url = `${proxyURL}https://api.opencovid.ca/summary?loc=${region}`;
+  let region = value === 'All' ? 'all' : value;
+  let url;
+
+  if(region === 'all') {
+    url = `${proxyURL}https://api.opencovid.ca/summary?geo=pt&pt_names=canonical&hr_names=canonical`;
+  } else {
+    url = `${proxyURL}https://api.opencovid.ca/summary?geo=hr&loc=${region}&pt_names=canonical&hr_names=canonical`;
+  }
+
   loadData(url);
-  console.log("url: " + url);
   headerRegion.innerText = regionDropdown.options[regionDropdown.selectedIndex].text;
 }
 
 // Initial loading of default option 'All' in dropdown lists
-function initDropdownList(dropdown){
+function initDropdownList(dropdown) {
   dropdown.length = 0;
   let defaultOption = document.createElement('option');
   defaultOption.text = 'All';
@@ -177,7 +190,7 @@ function initDropdownList(dropdown){
   dropdown.selectedIndex = 0;
 }
 
-function convertFromStringToDate(responseDate) {
-  let datePieces = responseDate.split("-");
-  return(new Date(datePieces[2], (datePieces[1]-1), datePieces[0]));
-}
+// function convertFromStringToDate(responseDate) {
+//   let datePieces = responseDate.split("-");
+//   return(new Date(datePieces[2], (datePieces[1]-1), datePieces[0]));
+// }
